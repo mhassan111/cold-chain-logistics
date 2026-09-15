a818a08322a4f5858ead2590226db41530b0ce15
mhassan111 <mhassanuetcs12@gmail.com>
2026-09-15 18:25:41 +0500

    CI/CD: AWS deployment

--- src/ui.py
+++ src/ui.py
@@ -124,7 +124,7 @@ if app_mode == "🧊 Dispatch Console":
     # ------------------------------------------
     # TAB 1: CHAT UI & AGENT EXECUTION
     # ------------------------------------------
-    st.title("Cold-Chain Incident Control Dashboard")
+    st.title("Cold-Chain Incident Control Dashboard(Project)")
     st.caption("Production Data Engineering Pipeline • Real-Time Decision Optimization Platform")
 
     for entry in st.session_state.ui_messages:
